dc930bdd627550cc93f230a5b1700ccc0ea3086a
dalmasmaka <smakadalma@gmail.com>
2023-06-09 00:06:32 +0200

    added contracts

--- ERD-Shop/ERD-Shop.User/Controllers/AuthenticationController.cs
+++ ERD-Shop/ERD-Shop.User/Controllers/AuthenticationController.cs
@@ -82,7 +82,7 @@ namespace ERD_Shop.User.Controllers
                 WishlistDto wishlist = new WishlistDto { ApplicationUserId = applicationUser.Id };
                 await _wishlistRepository.CreateWishlist(wishlist);
                 await _publishEndpoint.Publish(new ApplicationOrderUserCreated(applicationUser.Id, applicationUser.First_Name, applicationUser.Last_Name, applicationUser.BirthDate, (int)applicationUser.City_Id, applicationUser.Zip_Code, applicationUser.Address, applicationUser.Email, role));
-
+                await _publishEndpoint.Publish(new ApplicationStoreUserCreated(applicationUser.Id, applicationUser.First_Name, applicationUser.Last_Name, applicationUser.BirthDate, (int)applicationUser.City_Id, applicationUser.Zip_Code, applicationUser.Address, applicationUser.Email, role));
                 return StatusCode(StatusCodes.Status201Created, new ResponseDto { IsSuccess = true, 
                                                                                   Result = registrationUser,
                                                                                   Message = "User created successfully!" });
