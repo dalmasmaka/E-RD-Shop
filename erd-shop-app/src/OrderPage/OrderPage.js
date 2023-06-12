4b826ca37d567af1ed1d81e45fd369903e796be7
Rrezze <88327916+Rrezze@users.noreply.github.com>
2023-06-12 16:03:38 +0200

    payment

--- erd-shop-app/src/OrderPage/OrderPage.js
+++ erd-shop-app/src/OrderPage/OrderPage.js
@@ -1,48 +1,89 @@
-import React, {useState} from 'react'
-import {FaShippingFast} from 'react-icons/fa'
-import {MdPayment} from 'react-icons/md'
-import './OrderPageCss.css'
+import React, { useState, useEffect } from 'react';
+import { FaShippingFast } from 'react-icons/fa';
+import { MdPayment } from 'react-icons/md';
+import './OrderPageCss.css';
 import PaymentForm from '../OrderPage/PaymentForm/PaymentForm';
 
-
-
 const OrderPage = () => {
-    const [selectShippingMethod, setSelectShippingMethod] = useState(''); 
-    const [selectPaymentMethod, setSelectPaymentMethod] = useState(''); 
+  const [selectShippingMethod, setSelectShippingMethod] = useState('');
+  const [selectPaymentMethod, setSelectPaymentMethod] = useState('');
+  const [countdown, setCountdown] = useState(null);
+
+  useEffect(() => {
+    if (selectPaymentMethod !== 'option5') {
+      const randomNumber = Math.floor(Math.random() * 100) + 1; // Generates a random number between 1 and 10
+
+      let startNumber = 100;
+      const countdownInterval = setInterval(() => {
+        startNumber--;
+        setCountdown(startNumber);
+        if (startNumber === randomNumber) {
+          clearInterval(countdownInterval);
+        }
+      }, 50);
+
+      return () => clearInterval(countdownInterval);
+    }
+  }, [selectPaymentMethod]);
 
   return (
     <div className='order-page'>
-        <h1 className='order-title'>Complete your Order</h1>
-        <div className='order-container'>
-            <div className='flex-order-left'>
-                <div className='input-text-order'> <p className='enter-bill-address'>Shipping address:</p> <input className='order-address' /></div>
-                <div className="shipping-method">
-               <p className='shipping-method-select'><FaShippingFast />
-                <select className='shipp-select' value={selectShippingMethod} onChange={(e) => setSelectShippingMethod(e.target.value)}>
-                    <option value="option0">Shipping Method</option>
-                    <option value="option1">By Ship</option>
-                    <option value="option2">By Airplane</option>
-                    <option value="option3">By Car</option>
-                </select>
-                </p> 
-                </div>
-                <div className="payment-method">
-               <p className='payment-method-select'><MdPayment />
-                <select className='pay-select' value={selectPaymentMethod} onChange={(e) => setSelectPaymentMethod(e.target.value)}>
-                    <option value="option3">Payment Method</option>
-                    <option value="option4">Cash</option>
-                    <option value="option5">Card</option>
-                </select>
-                </p> 
-                </div>
-            </div>
-            <div className='flex-order-right'>
-                <PaymentForm/>
-            </div>
+      <h1 className='order-title'>Complete your Order</h1>
+      <div className='order-container'>
+        <div className='flex-order-left'>
+          <div className='input-text-order'>
+            <p className='enter-bill-address'>Shipping address:</p>{' '}
+            <input className='order-address' />
+          </div>
+          <div className='shipping-method'>
+            <p className='shipping-method-select'>
+              <FaShippingFast />
+              <select
+                className='shipp-select'
+                value={selectShippingMethod}
+                onChange={(e) => setSelectShippingMethod(e.target.value)}
+              >
+                <option value='option0'>Shipping Method</option>
+                <option value='option1'>By Ship</option>
+                <option value='option2'>By Airplane</option>
+                <option value='option3'>By Car</option>
+              </select>
+            </p>
+          </div>
+          <div className='payment-method'>
+            <p className='payment-method-select'>
+              <MdPayment />
+              <select
+                className='pay-select'
+                value={selectPaymentMethod}
+                onChange={(e) => setSelectPaymentMethod(e.target.value)}
+              >
+                <option value='option3'>Payment Method</option>
+                <option value='option4'>Cash</option>
+                <option value='option5'>Card</option>
+              </select>
+            </p>
+          </div>
         </div>
+        <div className='flex-order-right'>
+          {selectPaymentMethod === 'option5' ? (
+            <PaymentForm />
+          ) : (
+            <div className='arrival'>
+              <h1 className='arrival-info'>
+                Your order will arrive in 
+                <h1 className='arrival-info'>{countdown}</h1>
+                <h1 className='arrival-info'>days</h1> 
+              </h1>
+            </div>
+          )}
+        </div>
+      </div>
+      <button className='submit-button-order' type='submit'>
+        Submit
+      </button>
     </div>
-    
-  )
-}
+  );
+};
 
-export default OrderPage
+export default OrderPage;
--- erd-shop-app/src/ShoppingCart/ShoppingCart.js
+++ erd-shop-app/src/ShoppingCart/ShoppingCart.js
@@ -1,62 +1,94 @@
-import React from 'react'
+import React from 'react';
 import './ShoppingCartCss.css';
 import { MdShoppingCartCheckout } from 'react-icons/md';
-import {RiDeleteBin5Line} from 'react-icons/ri'
-import {TiDeleteOutline} from 'react-icons/ti'
+import { RiDeleteBin5Line } from 'react-icons/ri';
+import { TiDeleteOutline } from 'react-icons/ti';
 import { useNavigate } from 'react-router';
 
 const ShoppingCart = () => {
-    const navigate = useNavigate();
-    // Fetch product variants based on the productId
-    // You can make an API call or use any other method to fetch the variants
-    const handleGoToClick = () => {
-      navigate('/orderpage'); // Redirect to the product variants page
-    };
+  const navigate = useNavigate();
+  // Fetch product variants based on the productId
+  // You can make an API call or use any other method to fetch the variants
+  const handleGoToClick = () => {
+    navigate('/orderpage'); // Redirect to the product variants page
+  };
 
+  // Calculate the total price of all products
+  const calculateTotalPrice = () => {
+    // Replace this sample data with the actual logic to calculate the total price
+    const products = [
+      { id: 1, name: 'produkti 1', store: 'Dior', price: 200, quantity: 3 },
+      { id: 1, name: 'produkti 1', store: 'Dior', price: 100, quantity: 1 },
+
+      // Add more products as needed
+    ];
+
+    const totalPrice = products.reduce((total, product) => {
+      return total + product.price * product.quantity;
+    }, 0);
+
+    return totalPrice;
+  };
 
   return (
-    <div className="main-container">
-    <div className="header-container">
+    <div className='main-container'>
+      <div className='header-container'>
         <h2 className='title'>Shopping Cart</h2>
-
-    </div>
-    <div className="table-container">
+      </div>
+      <div className='table-container'>
         <table>
+          <thead>
             <tr>
-                <th>ID</th>
-                <th>Product</th>
-                <th>Store</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Actions</th>
-
+              <th>ID</th>
+              <th>Product</th>
+              <th>Store</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Actions</th>
             </tr>
+          </thead>
+          <tbody>
             <tr>
-                <td>1</td>
-                <td>
-                    <p>produkti 1</p>
-                </td>
-                <td>Dior</td>
-                <td>200$</td>
-                <td>3</td>{/*ktu duhet me vendos numrin qe e zgjedhim te detajet e product variantit */}
-                <td className='action-buttons'>
-                  
-                <button className='delete-product-shoppingcart'>   
-                         <TiDeleteOutline />
+              <td>1</td>
+              <td>
+                <p>produkti 1</p>
+              </td>
+              <td>Dior</td>
+              <td>200$</td>
+              <td>3</td>
+              <td className='action-buttons'>
+                <button className='delete-product-shoppingcart'>
+                  <TiDeleteOutline />
                 </button>
-                </td>
+              </td>
             </tr>
+            {/* Add more product rows as needed */}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan='3'></td>
+              <td className='total-price-label'>Total Price:</td>
+              <td colSpan='2' className='total-price-value'>
+                {calculateTotalPrice()}$
+              </td>
+            </tr>
+          </tfoot>
         </table>
+      </div>
+      <div className='order-button'>
+        <button className='goto-order-btn' onClick={handleGoToClick}>
+          <MdShoppingCartCheckout />
+          Order
+        </button>
+      </div>
+      <div className='deleteAll-button'>
+        <button className='delete-order-btn'>
+          <RiDeleteBin5Line />
+          Delete All
+        </button>
+      </div>
     </div>
-    <div className='order-button'>
-    <button className='goto-order-btn' onClick={handleGoToClick}><MdShoppingCartCheckout />Order</button>
-    </div>
-    <div className='deleteAll-button'>
-    <button className='delete-order-btn'><RiDeleteBin5Line />Delete All</button>
-    </div>
-    
-</div>
-  )
-}
+  );
+};
 
-export default ShoppingCart
+export default ShoppingCart;
