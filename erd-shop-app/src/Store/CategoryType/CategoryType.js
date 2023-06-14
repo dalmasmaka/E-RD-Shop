a37814bd5423d582a8c5c09e89889de037c84bc5
Rrezze <88327916+Rrezze@users.noreply.github.com>
2023-06-14 09:50:22 +0200

    create search

--- erd-shop-app/src/Store/CategoryType/CategoryType.js
+++ erd-shop-app/src/Store/CategoryType/CategoryType.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom'; 
 import './CategoryType.css';
-import ProductVariants from '../Product/Product'
+import Product from '../Product/Product'
 
 const CategoryType = ({ id, name, image }) => {
   const navigate = useNavigate();
