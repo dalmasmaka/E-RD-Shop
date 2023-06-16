a3d461a2bc6956a208369b46ae89f2f385aed9ba
Rrezze <88327916+Rrezze@users.noreply.github.com>
2023-06-16 14:08:45 +0200

    endpointHandling

--- erd-shop-app/src/Store/ProductVariant/ProductVariants.js
+++ erd-shop-app/src/Store/ProductVariant/ProductVariants.js
@@ -1,42 +1,54 @@
-import React from 'react';
-import iphone from '../Assets/img/iphone.png'
-import { useParams } from "react-router-dom"
-import './ProductVariantsCss.css';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import iphone from "../Assets/img/iphone.png";
+import { useParams } from "react-router-dom";
+import "./ProductVariantsCss.css";
+import { useNavigate } from "react-router-dom";
+import { getVariantsByProduct } from "../../API/api";
 
 const ProductVariants = ({ productid }) => {
+  const [products, setProducts] = useState([]);
 
   const navigate = useNavigate();
+  let { id } = useParams();
 
   // Function to handle the "Go To" button click
-  const handleGoToClick = () => {
+  const handleGoToClick = (id) => {
     navigate(`/productvariants/${id}`); // Redirect to the product variants page
   };
+  // useEffect(() => {
+  //   const fetchProductVariants = async () => {
+  //     const data = await getVariantsByProduct(id);
+  //     setProducts(data.result);
+  //   };
+  //   fetchProductVariants();
+  // }, []);
 
   const productVariants = [
-    { id: 1, name: 'iphone', image:iphone },
-    { id: 2, name: 'samsung', image:iphone },
-    { id: 3, name: 'iphone13', image:iphone},
-    { id: 4, name: 'samsung', image:iphone },
-    { id: 5, name: 'iphone13', image:iphone}
+    { id: 1, name: "iphone", image: iphone },
+    { id: 2, name: "samsung", image: iphone },
+    { id: 3, name: "iphone13", image: iphone },
+    { id: 4, name: "samsung", image: iphone },
+    { id: 5, name: "iphone13", image: iphone },
   ];
-  let { id } = useParams();
   return (
-    <div className='product-var'>
-      <h2 className='product-title'>Type</h2>
-      <div className='category-products'>
-      {productVariants.map((productVariant) => (
-        <div key={productVariant.id}>
-           <div className='product' onClick={handleGoToClick}>
-        <img alt='' src={productVariant.image} />
-        <div className='product-info'>
-          <p className='product-name'>{productVariant.name}</p>
-          <div className='btn-flex'>
-        </div>
-        </div>
-      </div></div>
-      ))}
-    </div>
+    <div className="product-var">
+      <h2 className="product-title">Type</h2>
+      <div className="category-products">
+        {productVariants.map((productVariant) => (
+          <div key={productVariant.id}>
+            <div
+              className="product"
+              onClick={() => handleGoToClick(productVariant.id)}
+            >
+              <img alt="" src={productVariant.image} />
+              <div className="product-info">
+                <p className="product-name">{productVariant.name}</p>
+                <div className="btn-flex"></div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
