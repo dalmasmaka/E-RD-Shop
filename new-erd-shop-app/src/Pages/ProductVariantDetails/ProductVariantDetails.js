c7dcc37945bad3dfa0dbe3277bded76f5ff45b09
Edi Krasniqi <93128181+EdiKrasniqi11@users.noreply.github.com>
2023-08-20 20:38:44 +0200

    adding and removing from wishlist/cart

--- new-erd-shop-app/src/Pages/ProductVariantDetails/ProductVariantDetails.js
+++ new-erd-shop-app/src/Pages/ProductVariantDetails/ProductVariantDetails.js
@@ -10,71 +10,183 @@ import {
   AiFillShopping,
 } from "react-icons/ai";
 import { useParams } from "react-router";
-import { getVariantDetails,getVariantsByProduct,getProductVariants,getStoreById } from "../../API/Api";
+import {
+  getVariantDetails,
+  getVariantsByProduct,
+  getProductVariants,
+  getStoreById,
+  getVariantsInUserWishlist,
+  getVariantsInUserShoppingCart,
+} from "../../API/Api";
 import { BASE_URL } from "../../API/Api";
-import { getVariantsInWishlist } from "../../API/Api";
-import { getVariantsInShoppingCart } from "../../API/Api";
 
 const ProductVariantDetails = () => {
-  const [isHeartFilled, setIsHeartFilled] = useState(false);
   const [isCartFilled, setIsCartFilled] = useState(false);
   const [count, setCount] = useState(0);
-  const [variant, setVariant] = useState({})
+  const [variant, setVariant] = useState({});
   const [relevantVariants, setRelevantVariants] = useState([]);
+  const [userId, setUserId] = useState("");
+  const [productExistsWishlist, setProductExistsWishlist] = useState(false);
+  const [productExistsCart, setProductExistsCart] = useState(false);
   const navigate = useNavigate();
-  // const [products, setProducts] = useState([]);
+  const params = useParams();
   const handleGoToClick = (id) => {
-    navigate(`/productvariants/${id}`); // Redirect to the product variants page
+    navigate(`/productvariants/${id}`);
   };
+  let { id } = useParams();
 
-  let {id}= useParams();
-  
-    useEffect(() => {
-      const fetchVariantDetails = async () => {
-        const data = await getVariantDetails(id);
-        setVariant(data.result);
-      };
-      fetchVariantDetails();
+  useEffect(() => {
+    const fetchVariantDetails = async () => {
+      const data = await getVariantDetails(id);
+      setVariant(data.result);
+    };
+    fetchVariantDetails();
   }, [id]);
+  useEffect(() => {
+    if (localStorage.getItem("access-token") != undefined) {
+      const userData = parseJwt(localStorage.getItem("access-token"));
+      setUserId(
+        userData["http://schemas.xmlsoap.org/ws/2009/09/identity/claims/actor"]
+      );
+    }
+  }, []);
+  useEffect(() => {
+    if (localStorage.getItem("access-token") != undefined) {
+      const userData = parseJwt(localStorage.getItem("access-token"));
+      setUserId(
+        userData["http://schemas.xmlsoap.org/ws/2009/09/identity/claims/actor"]
+      );
+    }
+  }, [params]);
 
+  //setting the wishlist and shopping cart values
+  useEffect(() => {
+    fetchUserWishlistData();
+    fetchUserCartData();
+  }, [userId]);
 
-  const handleAddToWishlist = (variant) => {
+  async function fetchUserWishlistData() {
+    try {
+      const userWishlist = await getVariantsInUserWishlist(userId);
+      const wishlistProductExists =
+        userWishlist.Result.find(
+          (product) => product.ProductVariantId == id
+        ) !== undefined;
+      setProductExistsWishlist(wishlistProductExists);
+
+      const userShoppingCart = await getVariantsInUserShoppingCart(userId);
+      console.log(userShoppingCart);
+      const shoppingCartProductExists =
+        userShoppingCart.Result.find(
+          (product) => product.ProductVariantId == id
+        ) !== undefined;
+      setProductExistsCart(shoppingCartProductExists);
+    } catch (error) {
+      console.error("ProductVariantDetails.js: line 85");
+    }
+  }
+
+  async function fetchUserCartData() {
+    try {
+      const userShoppingCart = await getVariantsInUserShoppingCart(userId);
+      const shoppingCartProductExists =
+        userShoppingCart.Result.find(
+          (product) => product.ProductVariantId == id
+        ) !== undefined;
+      setProductExistsCart(shoppingCartProductExists);
+    } catch (error) {
+      console.error("ProductVariantDetails.js: line 98");
+    }
+  }
+
+  function parseJwt(token) {
+    try {
+      var base64Url = token.split(".")[1];
+      var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+      var jsonPayload = decodeURIComponent(
+        window
+          .atob(base64)
+          .split("")
+          .map(function (c) {
+            return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+          })
+          .join("")
+      );
+      return JSON.parse(jsonPayload);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const handleAddToWishlist = async (variant) => {
     const url = `${BASE_URL}/WishlistManagement`;
     const requestData = {
-      variant: variant,
-      quantity: count
+      UserId: userId,
+      ProductId: variant.productVariantId,
     };
-    fetch(url, {
-      method: 'POST',
+    await fetch(url, {
+      method: "POST",
       body: JSON.stringify(requestData),
       headers: {
-          'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-  })
-  setIsHeartFilled(!isHeartFilled);
+    });
+    window.location.reload();
+  };
+
+  const handleDeleteFromWishlist = async (variant) => {
+    const url = `${BASE_URL}/WishlistManagement`;
+    const requestData = {
+      UserId: userId,
+      ProductId: variant.productVariantId,
+    };
+    await fetch(url, {
+      method: "DELETE",
+      body: JSON.stringify(requestData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    window.location.reload();
   };
 
   const handleAddToShoppingCart = (variant) => {
     const url = `${BASE_URL}/ShoppingCartManagement`;
     const requestData = {
-      variant: variant,
-      quantity: count
+      UserId: userId,
+      ProductId: variant.productVariantId,
     };
     fetch(url, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify(requestData),
       headers: {
-          'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-  })
-  setIsCartFilled(!isCartFilled);
+    });
+    window.location.reload();
+  };
+
+  const handleDeleteFromShoppingCart = (variant) => {
+    const url = `${BASE_URL}/ShoppingCartManagement`;
+    const requestData = {
+      UserId: userId,
+      ProductId: variant.productVariantId,
+    };
+    fetch(url, {
+      method: "DELETE",
+      body: JSON.stringify(requestData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    window.location.reload();
   };
 
   const incrementCount = () => {
-    if(count < variant.stockQuantity){
+    if (count < variant.stockQuantity) {
       setCount((prevCount) => prevCount + 1);
-    };
-  }
+    }
+  };
 
   const decrementCount = () => {
     if (count > 0) {
@@ -84,9 +196,11 @@ const ProductVariantDetails = () => {
   useEffect(() => {
     const fetchRelevantVariants = async () => {
       const data = await getProductVariants();
-      const filteredProductVariants = data.result.filter(
-        (productVariant) => productVariant.productId === variant.productId
-      ).filter((v) => v.productVariantId !== variant.productVariantId);
+      const filteredProductVariants = data.result
+        .filter(
+          (productVariant) => productVariant.productId === variant.productId
+        )
+        .filter((v) => v.productVariantId !== variant.productVariantId);
       setRelevantVariants(filteredProductVariants);
     };
     if (variant.productId) {
@@ -95,58 +209,72 @@ const ProductVariantDetails = () => {
   }, [variant]);
   return (
     <div className="all-container">
-    <div className="container">
-      <div className="leftDiv">
-        <img alt="" src={variant.productVariantImg}></img>
-      </div>
-      <div className="rightDiv">
-        <h2 className="variant-title">{variant.productVariantName}</h2>
-        <h4 className="variant-price">
-          Price:<span className="product-price">{variant.price}$</span>
-        </h4>
-        <p className="variant-category">
-          Description:<span className="category-type"> {variant.shortDescription}</span>
-        </p>
-        <div className="quantity">
+      <div className="container">
+        <div className="leftDiv">
+          <img alt="" src={variant.productVariantImg}></img>
+        </div>
+        <div className="rightDiv">
+          <h2 className="variant-title">{variant.productVariantName}</h2>
+          <h4 className="variant-price">
+            Price:<span className="product-price">{variant.price}$</span>
+          </h4>
+          <p className="variant-category">
+            Description:
+            <span className="category-type"> {variant.shortDescription}</span>
+          </p>
+          <div className="quantity">
+            <button
+              className="quantity-btn decrease-btn"
+              onClick={decrementCount}
+            >
+              <FaChevronLeft />
+            </button>
+            <span> {count} </span>
+            <button
+              className="quantity-btn increase-btn"
+              onClick={incrementCount}
+            >
+              <FaChevronRight />
+            </button>
+          </div>
           <button
-            className="quantity-btn decrease-btn"
-            onClick={decrementCount}
+            className="btn-flex-details btn-wishlist-details"
+            onClick={() =>
+              productExistsWishlist
+                ? handleDeleteFromWishlist(variant)
+                : handleAddToWishlist(variant)
+            }
           >
-            <FaChevronLeft />
+            {productExistsWishlist ? <AiFillHeart /> : <AiOutlineHeart />}
           </button>
-          <span> {count} </span>
           <button
-            className="quantity-btn increase-btn"
-            onClick={incrementCount}
+            className="btn-flex-details btn-shopping-details"
+            onClick={() =>
+              productExistsCart
+                ? handleDeleteFromShoppingCart(variant)
+                : handleAddToShoppingCart(variant)
+            }
           >
-            <FaChevronRight />
+            {productExistsCart ? <AiTwotoneShopping /> : <AiOutlineShopping />}
           </button>
         </div>
-        <button
-          className="btn-flex-details btn-wishlist-details"
-          onClick={() => handleAddToWishlist(variant)}
-        >
-          {isHeartFilled ? <AiFillHeart /> : <AiOutlineHeart />}
-        </button>
-        <button
-          className="btn-flex-details btn-shopping-details"
-          onClick={() => handleAddToShoppingCart(variant)}
-        >
-          {isCartFilled ? <AiTwotoneShopping /> : <AiOutlineShopping />}
-        </button>
       </div>
-    </div>
-    <h1 className="tekst-varianti">Similar Products</h1>
-    <div className="category-products variante">
+      <h1 className="tekst-varianti">Similar Products</h1>
+      <div className="category-products variante">
         {relevantVariants.map((productVariant) => (
-          <div className="products-variant" key={productVariant.productVariantId}>
+          <div
+            className="products-variant"
+            key={productVariant.productVariantId}
+          >
             <div
               className="product"
               onClick={() => handleGoToClick(productVariant.productVariantId)}
             >
               <img alt="" src={productVariant.productVariantImg} />
               <div className="product-info">
-                <p className="product-name">{productVariant.productVariantName}</p>
+                <p className="product-name">
+                  {productVariant.productVariantName}
+                </p>
                 <div className="btn-flex"></div>
               </div>
             </div>
